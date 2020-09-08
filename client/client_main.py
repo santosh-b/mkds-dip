c94ab2ef2b026136541e51b02caff88b652749eb
santosh-b <43103834+santosh-b@users.noreply.github.com>
2020-09-07 22:32:50 -0500

    initial commit

---
+++ client/client_main.py
@@ -0,0 +1,16 @@
+from desmume.emulator import DeSmuME
+
+def main(emu):
+    emu.open('assets/0168 - Mario Kart DS (U)(SCZ).nds')
+    win = emu.create_sdl_window()
+
+    frame = 0
+    while frame := frame+1:
+        emu.cycle()
+        win.draw()
+        win.process_input()
+
+
+if __name__ == '__main__':
+    emulator = DeSmuME()
+    main(emulator)
