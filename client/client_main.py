bcebd4f73d3ca1936c3c3aec3b6a3a0505113675
santosh-b <43103834+santosh-b@users.noreply.github.com>
2020-09-08 13:56:32 -0500

    training algorithm

--- client/client_main.py
+++ client/client_main.py
@@ -1,14 +1,25 @@
 from desmume.emulator import DeSmuME
+from client.trainer import Trainer
+import sys
+from contextlib import redirect_stdout
 
 def main(emu):
     emu.open('assets/0168 - Mario Kart DS (U)(SCZ).nds')
     win = emu.create_sdl_window()
+    emu.savestate.load_file('assets/lol.ds2')
+    emu.input.keypad_add_key(64)
+    emu.volume_set(0)
+    reward_function_addr = int('0x02354B1C', 16)
+    #while True:
+    #    emu.cycle()
+    #    win.draw()
+    #    win.process_input()
+    #    print(emu.memory.read(reward_function_addr, reward_function_addr, 2, signed=True))
 
-    frame = 0
-    while frame := frame+1:
-        emu.cycle()
-        win.draw()
-        win.process_input()
+    with open('assets/file.txt', 'w') as f:
+        with redirect_stdout(f):
+            trainer = Trainer(emu, win, int('0x02354B1C', 16), f)
+            trainer.train()
 
 
 if __name__ == '__main__':
---
+++ client/trainer.py
@@ -0,0 +1,55 @@
+import math
+
+LEFT = 32
+RIGHT = 16
+BRANCH_FRAMES = 60
+
+import contextlib
+
+class Trainer:
+
+    def __init__(self, emulator, window, reward, outputfile=None):
+        self.emu = emulator
+        self.win = window
+        self.reward_function_addr = reward
+        self.f = outputfile
+
+    def train(self):
+        self.emu.savestate.save(9)
+        self.emu.input.keypad_add_key(1)
+        for i in range(1000):
+            self.simulate_branches()
+
+    def simulate_branches(self):
+        # slot 9 stores the base savestate
+        # slot 8 stores the best branch savestate
+        base_save = self.emu.savestate.save(9)
+
+        # the possible branches of the Trainer AI
+        branches = [(60, 32), (50, 32), (40, 32),(30, 32), (20, 32), (10, 32), (0, 0),
+                    (60, 16), (50, 16), (40, 16),(30, 16), (20, 16), (10, 16)]
+        best_branch_reward = -math.inf
+        for t, dir in branches:
+            frame = 0
+            while (frame := frame+1) < BRANCH_FRAMES:
+                if frame <= t:
+                    self.emu.input.keypad_add_key(dir)
+                else:
+                    self.emu.input.keypad_rm_key(dir)
+                self.emu.cycle()
+                self.win.draw()
+            branch_reward = self.emu.memory.read(self.reward_function_addr,self.reward_function_addr,2,signed=True)
+            if(self.f):
+                print('attempted reward',branch_reward)
+                self.f.flush()
+            if branch_reward > best_branch_reward:
+                best_branch_reward = branch_reward
+                self.emu.savestate.save(8)
+            self.emu.savestate.load(9)
+        self.emu.savestate.load(8)
+        self.emu.savestate.save(9)
+        if(self.f):
+            print('best reward',best_branch_reward,'------\n')
+            self.f.flush()
+
+        return best_branch_reward
