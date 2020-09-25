8487b493820d5da6c1d63b76cc5ddfca1683b523
santosh-b <43103834+santosh-b@users.noreply.github.com>
2020-09-25 00:53:24 -0500

    NOW FEATURING: automated training-data generation via self-supervision and time travel!

--- client/client_main.py
+++ client/client_main.py
@@ -1,25 +1,48 @@
 from desmume.emulator import DeSmuME
-from client.trainer import Trainer
+from client.teacher import Teacher
 import sys
 from contextlib import redirect_stdout
 
 def main(emu):
     emu.open('assets/0168 - Mario Kart DS (U)(SCZ).nds')
     win = emu.create_sdl_window()
-    emu.savestate.load_file('assets/lol2.ds2')
+    emu.NB_STATES = 100
+    emu.savestate.load_file('assets/shroom.dst')
+
     emu.input.keypad_add_key(64)
     emu.volume_set(0)
     reward_function_addr = int('0x02354B1C', 16)
-    # while True:
-    #    emu.cycle()
-    #    win.draw()
-    #    win.process_input()
-    #    print(emu.memory.read(reward_function_addr, reward_function_addr, 2, signed=True))
+    # frame = 0
+    # while frame := frame+1:
+    #     emu.cycle()
+    #     win.draw()
+    #     win.process_input()
+    #     #if frame==1000:
+    #     #    cheat(emu)
+    #     #    print('chaeated')
+    #     #print(emu.memory.read(int('0x023599DC', 16), int('0x023599DC', 16), 2, signed=True))
 
-    with open('assets/file.txt', 'w') as f:
-        with redirect_stdout(f):
-            trainer = Trainer(emu, win, int('0x0233EF5C', 16), f)
-            trainer.train()
+    #print(emu.input.keypad_get()) #0x0233EF5C
+    # peach gardens time trial 0235EB1C
+    # shroom ridge time trial 023599DC
+
+    with open('assets/file.txt', 'w') as logfile, open('training_data/labels.txt','a') as labelfile:
+        with redirect_stdout(logfile):
+            teacher = Teacher(emu, win, int('0x023599DC', 16), logfile, labelfile)
+            teacher.train()
+
+def cheat(emu):
+    '''
+    92056E14 00004002
+    12056E12 000020FF
+    D2000000 00000000
+    '''
+    emu.memory.write_long(int('0x02056E14', 16), int('0x4002', 16))
+    emu.memory.write_long(int('0x02056E12', 16), int('0x20FF',16))
+    #emu.memory.write_long(int('0x02056E12', 16), int('0x20FF', 16))
+    #print(hex(emu.memory.read(int('0x02056E12', 16),int('0x02056E12', 16),2,signed=False)))
+    #print(hex(emu.memory.read(int('0x02056E14', 16), int('0x02056E14', 16), 2, signed=False)))
+
 
 
 if __name__ == '__main__':
---
+++ client/teacher.py
@@ -0,0 +1,130 @@
+import math
+import numpy as np
+
+LEFT = 32
+RIGHT = 16
+BRANCH_FRAMES = 30
+
+BASE_SAVESTATE = 99
+
+class Teacher:
+
+    def __init__(self, emulator, window, reward, outputfile=None, labelfile=None):
+        self.emu = emulator
+        self.win = window
+        self.reward_function_addr = reward
+        self.f = outputfile
+        self.l = labelfile
+        self.road_colors = ((192, 192, 192), (112, 104, 80))
+        self.screen_watch_ratio = 0.7
+        self.branch_pmf = [.9, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .00, .00, .00, .00]
+        #self.branch_pmf = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        #peach gardens ((192,200,200),(128,96,56),(240,232,176),(216,216,216))
+        #shroom ridge ((192,192,192),(112,104,80))
+
+    def train(self):
+        self.emu.input.keypad_add_key(1)
+        self.emu.savestate.save(BASE_SAVESTATE)
+        delta_progress = []
+        for t in range(1000):
+            # simulate all the branches at this timestep and get the resulting scores
+            pre_progress_score = self.get_reward_memory()
+            branch_scores = self.simulate_branches()
+
+            # choose the best score, make it the BASE_STATE for the next timestep t+1, and proceed
+            # best_branch = branch_scores[0]
+            # Sometimes we intentially choose a suboptimal branch to diversify our training data!
+            rank_choose = np.random.choice(np.arange(15), p=self.branch_pmf)
+            next_branch = branch_scores[rank_choose]
+            self.emu.savestate.load(next_branch[2])
+            self.emu.savestate.save(BASE_SAVESTATE)
+            post_progress_score = self.get_reward_memory()
+
+            if len(delta_progress) > 2 and delta_progress[-1] <= 0 and delta_progress[-2] <= 0 and delta_progress[-3] <= 0:
+                break
+
+            if rank_choose == 0:
+                delta_progress.append(post_progress_score - pre_progress_score)
+                if delta_progress[-1] > 0:
+                    self.dump_data(branch_scores[0][1])
+            else:
+                self.l.write('skipped dump\n')
+                self.l.flush()
+
+            if self.f:
+                print(f'chose {rank_choose} branch')
+                print('current progress', self.get_reward_memory())
+                print(delta_progress)
+                print('chosen reward', next_branch[0], '------\n')
+                self.f.flush()
+
+    def dump_data(self, label):
+        key = np.datetime64('now').astype(int)
+        self.emu.screenshot().save(f'training_data/{key}.png')
+        self.l.write(f'{key}; {label}\n')
+        self.l.flush()
+
+    def color_dist(self, c1, c2):
+        return np.min(np.linalg.norm(c1-c2,axis=1))
+
+    def get_reward_memory(self):
+        return self.emu.memory.read(self.reward_function_addr,self.reward_function_addr,2,signed=True)
+
+    def get_current_reward(self):
+        reward = self.get_reward_memory()
+        pixels_bot = np.asarray(self.emu.screenshot().crop((127, 250, 128, 280)))
+        pixels_top = np.asarray(self.emu.screenshot().crop((127, 69, 128, 108)))
+        # Scan for pixels in the top screen
+        for pix in pixels_top:
+            pix = pix[0]
+            dist = self.color_dist(pix,self.road_colors)
+            if dist > 40:
+                reward -= 2*self.screen_watch_ratio
+        # Scan for pixels in the bottom screen
+        for pix in pixels_bot:
+            pix = pix[0]
+            dist = self.color_dist(pix,self.road_colors)
+            if dist > 40:
+                reward -= 2*(1-self.screen_watch_ratio)
+        return reward
+
+    def simulate_branch(self, branch):
+        t, dir = branch
+        frame = 0
+
+        while (frame := frame+1) < BRANCH_FRAMES:
+            if frame <= t:
+                self.emu.input.keypad_add_key(dir)
+            else:
+                self.emu.input.keypad_rm_key(dir)
+            self.emu.cycle()
+            self.win.draw()
+
+        branch_reward = self.get_current_reward()
+        if dir == 0:
+            # score the branch if it only goes straight, i.e. the 'safe' option
+            branch_reward += 5
+        if self.f:
+            # log results
+            #print('attempted reward', branch_reward)
+            self.f.flush()
+
+        return branch_reward
+
+    def simulate_branches(self):
+        # the possible branches of the Trainer AI
+        branches = [(30, 32), (30, 32), (25, 32), (20, 32),(15, 32), (10, 32), (5, 32), (0, 0),
+                    (5, 16), (10, 16), (15, 16), (20, 16), (25, 16), (30, 16), (30, 16)]
+        branch_scores = []
+
+        for savestate_slot, branch in enumerate(branches):
+            # Simulate the branch, log the branch score
+            branch_score = self.simulate_branch(branch)
+            branch_scores.append((branch_score, branch, savestate_slot))
+            self.emu.savestate.save(savestate_slot)
+            # Reload the base savestate and retry
+            self.emu.savestate.load(BASE_SAVESTATE)
+
+        branch_scores = sorted(branch_scores, key=lambda x: x[0], reverse=True)
+
+        return branch_scores
